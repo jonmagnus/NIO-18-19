4c3bb7c668af525dbbb2db1f9086de546a247ebd
Jon-Magnus Rosenblad <jonmagnusr@gmail.com>
2019-01-07 08:57:56 +0100

    Completed pizzatur

--- pizzatur.cpp
+++ pizzatur.cpp
@@ -1,2 +1,28 @@
 /* Gå nedover listen og regn ut manhatten-avstanden mellom punktene. Legg
- * så til 15*N.*/
+ * så til 15*N.
+ * Score 100/100*/
+#include <cstdio>
+using namespace std;
+
+#define MAX_N 100005
+#define abs(A) ((A) < 0 ? -(A) : (A))
+typedef long long ll;
+
+int N,x[MAX_N],y[MAX_N];
+
+int main()
+{
+	while (scanf("%d ", &N) != EOF)
+	{
+		ll sum = 15*N;
+		for (int i = 0; i < N; i++)
+		{
+			scanf("%d %d ", &x[i], &y[i]);
+			sum += (i ? abs(x[i] - x[i-1]) + abs(y[i] - y[i-1]) : 0);
+		}
+
+		printf("%lld\n", sum);
+	}
+
+	return 0;
+}
